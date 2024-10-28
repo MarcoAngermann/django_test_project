a1e4d1ff507cfce1f97b057cc9d68d79ceb5c65e
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-10-28 09:20:26 +0100

    httpresponse json

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+
+from .dummy_data import gadgets
 
 # Create your views here.
 
 def start_page_view(request):
     return HttpResponse('Hello, World!')
+
+def single_gadget_view(request):
+    return HttpResponse(json.dumps(gadgets[0]), content_type='application/json')
